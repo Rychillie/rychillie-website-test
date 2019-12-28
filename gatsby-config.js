75dfeac5b2ac8334600f61e6333008b18c58d474
Rychillie Umpierre de Oliveira <rychillie@hotmail.com>
2019-12-28 01:37:55 -0300

    agr true

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
-        enableIdentityWidget: false,
+        enableIdentityWidget: true,
       },
     },
     `gatsby-plugin-react-helmet`,
