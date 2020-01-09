325a012eeb556b7d813ab3162776a094f1126815
Rychillie <rychillie@hotmail.com>
2020-01-09 12:55:30 -0300

    teste-ui

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,38 @@
 import {Link} from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import logo from "../../static/images/logo.png"
+
+//import iconSearch from "../../static/images/Search-Icon.svg"
 
 const Header = ({siteTitle}) => (
     <header>
         <div className={"container"}>
-            <div className={"top-menu"}>
-                <div className={"logo"}>
-                    <Link to="/" title={"Gatsby Starter"}>
-                        <img alt={"Logo"} src={logo}/>
-                    </Link>
+            <div className={"row top-menu"}>
+                <div className={"col-10-sm col-8 title"}>
+                    <Link to="/" title={siteTitle} className={"title"}>{siteTitle}</Link>
                 </div>
 
-                <div className={"get-started"}>
-                    <a href="https://github.com/app-generator/gatsby-starter-scss">Get Sources</a>
-                </div>
+                <nav className={"col-2-sm col-4 nav-top"}>
+                    <input id="menu-hamburguer" type="checkbox"/>
+
+                    <label for="menu-hamburguer">
+                        <div class="menu">
+                            <span class="hamburguer"></span>
+                        </div>
+                    </label>
+
+                    <ul>
+                        <li>
+                            <Link to="/blog" title={"Blog"}>Blog</Link>
+                        </li>
+                        <li>
+                            <Link to="/blog" title={"Blog"}>Portfolio</Link>
+                        </li>
+                        <li>
+                            <Link to="/blog" title={"Blog"}>About</Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </header>
@@ -23,10 +40,6 @@ const Header = ({siteTitle}) => (
 
 Header.propTypes = {
     siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-    siteTitle: ``,
 }
 
 export default Header
