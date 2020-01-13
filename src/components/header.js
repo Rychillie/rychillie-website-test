d166f7ceff558020abff288aad5991a16d24edf6
Rychillie Umpierre de Oliveira <rychillie@hotmail.com>
2020-01-13 00:01:25 -0300

    text

--- src/components/header.js
+++ src/components/header.js
@@ -35,17 +35,17 @@ const Header = ({siteTitle}) => (
                     </ul>
                 </nav>
             </div>
-        </div>
 
-        <div className={"apresentation row"}>
-            <div className={"col-3 profile"}>
-                <img alt="Rychillie" src={ProfileImg}/>
-            </div>
-            <div className={"col-9 hometext"}>
-                <h2>
-                    Olá, Eu sou <span>Rychillie</span>
-                </h2>
-                <p>Um jovem Programador Front-End querendo compartilhar seu conhecimento. Sempre buscando evoluir e melhorar em todos os aspectos. Venha <Link to="/about" title={"About"}>me Conhecer</Link> um pouco mais e o <Link to="/portfolio" title={"Portfolio"}>meu trabalho</Link>.</p>
+            <div className={"apresentation row"}>
+                <div className={"col-3 profile"}>
+                    <img alt="Rychillie" src={ProfileImg}/>
+                </div>
+                <div className={"col-9 hometext"}>
+                    <h2>
+                        Olá, Eu sou <span>Rychillie</span>
+                    </h2>
+                    <p>Um jovem Programador Front-End querendo compartilhar seu conhecimento. Sempre buscando evoluir e melhorar em todos os aspectos. Venha <Link to="/about" title={"About"}>me Conhecer</Link> um pouco mais e o <Link to="/portfolio" title={"Portfolio"}>meu trabalho</Link>.</p>
+                </div>
             </div>
         </div>
     </header>
