923580e7e75ea4208d1f1ae375b996ae7765152b
Rychillie <rychillie@hotmail.com>
2020-01-21 10:31:59 -0300

    adsense

---
+++ src/components/adsense.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+const Adsense = () => (
+    <div className={"adsense"}>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-7837358846130941"
+        data-ad-slot="9265933715"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
+)
+
+export default Adsense
+
+
+
--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <script data-ad-client="ca-pub-7837358846130941" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
